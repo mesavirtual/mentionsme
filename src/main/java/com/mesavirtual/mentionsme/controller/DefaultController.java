071bf992feb0c66615be88c513d388ea0d79c80d
Marcelo Oliveira <marcelo@marceloweb.info>
2022-02-24 19:37:25 -0300

    added default controller

---
+++ src/main/java/com/mesavirtual/mentionsme/controller/DefaultController.java
@@ -0,0 +1,12 @@
+package com.mesavirtual.mentionsme.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DefaultController {
+
+    public String index() {
+        return "Hello World";
+    }
+
+}

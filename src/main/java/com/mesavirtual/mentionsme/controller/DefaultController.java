ce7b7a0b92352a4c8165d8e812ee16a8f69b3b67
Marcelo Oliveira <marcelo@marceloweb.info>
2022-02-25 11:03:17 -0300

    view and routes configured

--- src/main/java/com/mesavirtual/mentionsme/controller/DefaultController.java
+++ src/main/java/com/mesavirtual/mentionsme/controller/DefaultController.java
@@ -1,12 +1,24 @@
 package com.mesavirtual.mentionsme.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class DefaultController {
 
-    public String index() {
-        return "Hello World";
+    @GetMapping("/")
+    public String index(@RequestParam(name="name", required=false, defaultValue="Login") String name, Model model) {
+        model.addAttribute("name", name);
+        return "default/index";
     }
 
+    @GetMapping("/home")
+    public String dashboard(@RequestParam(name="name", required=false, defaultValue="Dashboard") String name, Model model) {
+        model.addAttribute("name", name);
+        return "default/home";
+    }
 }
